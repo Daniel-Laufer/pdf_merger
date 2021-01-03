4bef7a06e118dc950ed61fb481a7694c3203ce89
Daniel Laufer <lauferkdaniel@gmail.com>
2021-01-02 22:26:50 -0500

    added method that allows users to merge in the order of the creation dates of the files

--- pdf_merger.py
+++ pdf_merger.py
@@ -3,6 +3,9 @@ from PyPDF2 import PdfFileMerger, PdfFileReader
 from os.path import expanduser
 from typing import *
 import re
+import pathlib
+import datetime
+
 
 
 DEV_PDFS = "./pdfs"
@@ -11,7 +14,7 @@ DEV_MODE = True
 
 def merge_custom_order(files: List[object], path_of_dir: str, merger: PdfFileMerger) -> bool:
     """
-    Merge the pdfs in a custom order which is specified by the user.
+    Merge the pdfs located at path_of_dir in a custom order which is specified by the user.
     
     For example, the string "512" will merge the fifth, first, and second files
     together in that exact order.
@@ -24,7 +27,6 @@ def merge_custom_order(files: List[object], path_of_dir: str, merger: PdfFileMer
         order = input("Specify an order to merge in: (for example, \"132\")")
     
     for char in order:
-        print(len(files))
         if 1 <= int(char) <= len(files) + 1:
             merger.append(PdfFileReader(files[int(char) - 1], strict=False))
         else:
@@ -38,26 +40,44 @@ def print_seperator() -> None:
     """
     print("\n" + "*"*100 +  "\n") 
 
-
 def choose_which_merge(files: List[object], path_of_dir: str) -> bool:
     """
     This method chooses which type of merge to do based on the user's input.
-    Return true if the files were successfully merged, false otherwise.
+    Return true if the files at path_of_dir were successfully merged, false otherwise.
     """
 
     choice = input(
         """What order would you like to merge the files in? Please type the corresponding number of your choice:\n
         (1)Some or all in a custom order (ex. 132 (first, then third, then second))
         \n\t(2)All PDFs in the way they appear in the directory
-        \n\t(3)All PDFS from Oldest to newest
-        \n\t(4)All PDFs Alphabetically
-        \n\t(5)All PDFs from Newest to Oldest\n        
+        \n\t(3)All PDFS from Oldest to newest modifications
+        \n\t(4)All PDFs from Newest to Oldest modifications
+        \n\t(5)All PDFS from Oldest to newest creation dates
+        \n\t(6)All PDFs from Newest to Oldest creation dates\n 
+        \n\t(7)All PDFs Alphabetically
+               
         """)
     
     merger = PdfFileMerger(strict=False)
-    
+    success = False
+
+
     if(choice.strip() in ['1', '(1)']):
         success = merge_custom_order(files, path_of_dir, merger)
+    elif(choice.strip() in ['2', '(2)']):
+        success = same_order_as_listed(files, path_of_dir, merger)
+    elif(choice.strip() in ['3', '(3)']):
+        success = merge_by_date_of_last_creation(files, path_of_dir, merger, True)
+    elif(choice.strip() in ['4', '(4)']):
+        success = merge_by_date_of_last_creation(files, path_of_dir, merger, False)
+    elif(choice.strip() in ['5', '(5)']):
+        success = merge_by_date_of_last_creation(files, path_of_dir, merger, True)
+    elif(choice.strip() in ['6', '(6)']):
+        success = merge_by_date_of_last_creation(files, path_of_dir, merger, False)
+    elif(choice.strip() in ['7', '(7)']):
+        success = merge_alphabetically(files, path_of_dir, merger)
+    
+
             
 
     if success:
@@ -67,11 +87,62 @@ def choose_which_merge(files: List[object], path_of_dir: str) -> bool:
             name += ".pdf"
         merger.write(path_of_dir + "/" + name)
         return True
+
     print_seperator()
     print("An error occurred.")
     return False
 
-        
+
+
+
+def merge_by_date_of_last_modification(files: List[object], path_of_dir: str, merger: PdfFileMerger, old_to_new: True) -> bool:
+    """
+    Merge the files located at path_of_dir in the order of the file that was more recently 
+    modified to the file that was modified first if old_to_new is False, if old_to_new is True 
+    then the files are merged in the opposite order. 
+
+    """
+    if(old_to_new):
+        files.sort(key=lambda x: os.path.getmtime(x.name))
+    else:
+         files.sort(key=lambda x: os.path.getmtime(x.name), reverse=True)
+
+
+    for file in files:
+        try:
+            merger.append(file)
+        except:
+            return False
+    return True
+
+
+def merge_by_date_of_last_creation(files: List[object], path_of_dir: str, merger: PdfFileMerger, old_to_new: True) -> bool:
+    """
+    Merge the files located at path_of_dir in the order of oldest to newest in terms of their creation dates if old_to_new is False, if 
+    old_to_new is True then that aforementioned order is reversed.
+
+    """
+    if(old_to_new):
+        files.sort(key=lambda x: os.path.getctime(x.name))
+    else:
+         files.sort(key=lambda x: os.path.getctime(x.name), reverse=True)
+
+
+
+    for file in files:
+        try:
+            merger.append(file)
+        except:
+            return False
+    return True
+
+
+    
+
+    
+    
+
+
 
 
 
@@ -96,14 +167,21 @@ if __name__ == "__main__":
     pdf_counter = 1
     for i, filename in enumerate(os.listdir(path_of_dir)):
         if filename.endswith(".pdf"):
+            complete_file_path = path_of_dir + "/" + filename
             try:
-                print("\t" + str(pdf_counter) + '. ' + filename)
-                pdf_files.append(open(path_of_dir + "/" + filename, "rb"))
+                fname = pathlib.Path(complete_file_path)
+                last_modified = datetime.datetime.fromtimestamp(fname.stat().st_mtime)
+                creation_date = datetime.datetime.fromtimestamp(fname.stat().st_ctime)
+                print("\t" + str(pdf_counter) + '. ' + filename  + "Created at " + str(creation_date) + ". Last modified at " + str(last_modified))
+                pdf_files.append(open(complete_file_path, "rb"))
                 pdf_counter += 1
             except:
                 print("Error creating file object for file " + filename)
 
     
+
+    # for file in pdf_files:
+    #     print(os.path.getctime(file.name))
     choose_which_merge(pdf_files, path_of_dir)
 
         
