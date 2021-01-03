5cff4c917d900ef90449e38560de4dc120b9036e
Daniel Laufer <lauferkdaniel@gmail.com>
2021-01-02 21:39:42 -0500

    fixed an issue where the numbering of the files was wrong. Also used regex to ensure that they use entered a valid order for the files to be merged in.

--- pdf_merger.py
+++ pdf_merger.py
@@ -2,10 +2,30 @@ import os
 from PyPDF2 import PdfFileMerger, PdfFileReader
 from os.path import expanduser
 from typing import *
+import re
 
 
 DEV_PDFS = "./pdfs"
 DEV_MODE = True
+
+
+def merge_custom_order(files: List[object], path_of_dir: str, merger: PdfFileMerger) -> bool:
+    print_seperator()
+    order = input("Specify an order to merge in: (for example, \"132\")")
+    while(not re.search("^[1-9]{2,}$", order)):
+        order = input("Specify an order to merge in: (for example, \"132\")")
+    
+    for char in order:
+        print(len(files))
+        if 1 <= int(char) <= len(files) + 1:
+            merger.append(PdfFileReader(files[int(char) - 1], strict=False))
+        else:
+            print("Error! Please try again!")
+            return False
+    return True
+
+def print_seperator() -> None:
+    print("\n" + "*"*100 +  "\n") 
 
 
 def merge_pdfs(files: List[object], path_of_dir: str) -> bool:
@@ -13,29 +33,32 @@ def merge_pdfs(files: List[object], path_of_dir: str) -> bool:
     Merges the PDFS together.
     Return true if the files were successfully merged, false otherwise.
     """
-    choice = input("What order would you like to merge the files in? Please type the corresponding number of your choice:\n(1)Some or all in a custom order (ex. 132 (first, then third, then second)) \n(2)All PDFs in the way they appear in the directory\n(3)All PDFS from Oldest to newest(4)All PDFs Alphabetically\n(5)All PDFs from Newest to Oldest\n")
-    merger = PdfFileMerger()
+
+    choice = input(
+        """What order would you like to merge the files in? Please type the corresponding number of your choice:\n
+        (1)Some or all in a custom order (ex. 132 (first, then third, then second))
+        \n\t(2)All PDFs in the way they appear in the directory
+        \n\t(3)All PDFS from Oldest to newest
+        \n\t(4)All PDFs Alphabetically
+        \n\t(5)All PDFs from Newest to Oldest\n        
+        """)
+    
+    merger = PdfFileMerger(strict=False)
     
     if(choice.strip() in ['1', '(1)']):
-        order = input("Specify an order to merge in: (for example, \"132\")")
-        try:
-            int(order)
-        except:
-            print("Error! Please try again")
-            choice = input("What order would you like to merge the files in? Please type the corresponding number of your choice:\n(1)Some or all in a custom order (ex. 132 (first, then third, then second)) \n(2)All PDFs in the way they appear in the directory\n(3)All PDFS from Oldest to newest(4)All PDFs Alphabetically\n(5)All PDFs from Newest to Oldest\n")
-        
-        for char in order:
-            if 1 <= int(char) <= len(files):
-                merger.append(PdfFileReader(files[int(char) - 1], strict=False))
-            else:
-                print("Error! Please try again!")
-                return False
+        success = merge_custom_order(files, path_of_dir, merger)
             
 
+    if success:
+        print_seperator()
         name = input("Success! Please choose a name for your new file: ")
         if not name.endswith(".pdf"):
             name += ".pdf"
         merger.write(path_of_dir + "/" + name)
+        return True
+    print_seperator()
+    print("An error occurred.")
+    return False
 
         
 
@@ -59,11 +82,13 @@ if __name__ == "__main__":
     print("Files found: ")
     # create the file objects
     pdf_files = []
-    for filename in os.listdir(path_of_dir):
+    pdf_counter = 1
+    for i, filename in enumerate(os.listdir(path_of_dir)):
         if filename.endswith(".pdf"):
             try:
-                print("\t" + filename)
+                print("\t" + str(pdf_counter) + '. ' + filename)
                 pdf_files.append(open(path_of_dir + "/" + filename, "rb"))
+                pdf_counter += 1
             except:
                 print("Error creating file object for file " + filename)
 
